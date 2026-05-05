1be129f7eca6235cbddde84e6a09551bf53dbee4
darun000 <reachoutdarun@gmail.com>
2026-05-06 01:02:58 +0530

    UC4: Maintain ordered train consist using LinkedList

---
+++ src/UseCase4TrainApp.java
@@ -0,0 +1,24 @@
+import java.util.LinkedList;
+
+public class UseCase4TrainApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=== Train Consist Management App ===");
+
+        LinkedList<String> train = new LinkedList<>();
+
+        train.add("Engine");
+        train.add("Sleeper");
+        train.add("AC");
+        train.add("Cargo");
+        train.add("Guard");
+
+        train.add(2, "Pantry");
+
+        train.removeFirst();
+        train.removeLast();
+
+        System.out.println("Final Train Consist: " + train);
+    }
+}
