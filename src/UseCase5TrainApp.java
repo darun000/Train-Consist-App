9130c48cc073b59a8cd270ed0e49995a82db6ef2
darun000 <reachoutdarun@gmail.com>
2026-05-06 01:14:46 +0530

    UC5: Preserve insertion order using LinkedHashSet

---
+++ src/UseCase5TrainApp.java
@@ -0,0 +1,20 @@
+import java.util.LinkedHashSet;
+
+public class UseCase5TrainApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=== Train Consist Management App ===");
+
+        LinkedHashSet<String> train = new LinkedHashSet<>();
+
+        train.add("Engine");
+        train.add("Sleeper");
+        train.add("Cargo");
+        train.add("Guard");
+
+        train.add("Sleeper");
+
+        System.out.println("Train Formation: " + train);
+    }
+}
